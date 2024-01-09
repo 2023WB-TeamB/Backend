d25fdc1ef671ea784f99c73fa483ec72208d7629
HongSeonah <rse1186@naver.com>
2024-01-09 14:45:46 +0900

    chore: requirements.txt 모듈 추가

--- docs/urls.py
+++ docs/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("create/", docs_create, name="docs_create"),
-    path("share/", docs_share, name="docs_share"),
+    path("docs/share/", docs_share, name="docs_share"),
 ]
