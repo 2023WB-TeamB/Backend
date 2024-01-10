046f880b655444c6f21d8149e249419fc2024c41
HongSeonah <rse1186@naver.com>
2024-01-11 00:36:35 +0900

    feat: 컨트리뷰터 API 구현

--- docs/urls.py
+++ docs/urls.py
@@ -8,4 +8,5 @@ urlpatterns =[
     # path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
     path("docs/create/", docs_create, name="docs_create"),
     path("docs/share/", docs_share, name="docs_share"),
+    path('docs/contributor/', docs_contributer, name='docs_contributer'),
 ]
