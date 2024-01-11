5398f6147eb8d8175f18e4ffdde154c9990801ea
kimsunjae <woal9844@gmail.com>
2024-01-11 19:49:01 +0900

    fix : 문서조회 api endpoint 수정

--- docs/urls.py
+++ docs/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns =[
     # path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
-    path('docs/<int:user_id>/', views.DocsList.as_view()), # get - 문서 내역 조회
+    path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
     # path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
     path("docs/create/", docs_create, name="docs_create"),
     path("docs/share/", docs_share, name="docs_share"),
