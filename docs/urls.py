0c354f715e0c99c77b6a252db1c5eeba94571a9b
kimsunjae <woal9844@gmail.com>
2024-01-11 20:22:27 +0900

    fix : 문서 생성 url path 변경

--- docs/urls.py
+++ docs/urls.py
@@ -6,7 +6,7 @@ urlpatterns =[
     # path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
     path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
     # path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
-    path("docs/create/", docs_create, name="docs_create"),
+    path("docs/create/", DocsCreateView.as_view()),
     path("docs/share/", docs_share, name="docs_share"),
     path('docs/contributor/', docs_contributor, name='docs_contributor'),
 ]
