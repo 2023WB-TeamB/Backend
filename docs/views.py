e591c0a6272a322d41329c738848dc6168776697
SNOW <kancth03@gmail.com>
2024-01-12 00:17:40 +0900

    feat: JWT 연결 및 RabbitMQ Celery 연결

--- docs/views.py
+++ docs/views.py
@@ -70,31 +70,79 @@ class DocsCreateView(APIView):
         language = request.data.get('language')
         color = request.data.get('color')
 
+        authorization_header = request.META.get('HTTP_AUTHORIZATION')
+        if authorization_header and authorization_header.startswith('Bearer '):
+            token = authorization_header.split(' ')[1]
+            user_id = user_token_to_data(token)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
         if not User.objects.filter(id=user_id).exists():  # user_id가 User 테이블에 존재하지 않는 경우
             return Response({
                 "status": 404,
                 "message": "user_id가 존재하지 않습니다.",
             }, status=status.HTTP_404_NOT_FOUND)
 
-        docs = Docs.objects.filter(is_deleted=False, user_id=user_id)  # is_deleted가 False인 객체만 조회
-
         if repository_url is None or language is None or language not in ['KOR', 'ENG'] or color is None:
             return Response({"message": "잘못된 요청입니다. 입력 형식을 확인해 주세요.", "status": 400},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        request.data['user_id'] = User.objects.filter(id=1).first().id
-        request.data['title'] = "OPGC (Open Source Project's Github Contributions)"
-        request.data['content'] = ""
-        request.data['user_id'] = User.objects.filter(id=user_id).first().id
-        request.data['title'] = "OPGC (Open Source Project's Github Contributions)"
-        request.data['content'] = """## 프로젝트 소개
-OPGC 프로젝트는 Github 프로필을 분석하여 사용자의 기여를 추적하고 랭킹을 매기는 서비스입니다."""
+        if url_validator(repository_url) is False:
+            return Response({"message": "유효하지 않은 URL입니다.", "status": 404}, status=status.HTTP_400_BAD_REQUEST)
 
+        framework = framework_finder(repository_url)
+        # Github, OpenAI API 이용해 Framework 추출
+        # return Response({"message": framework, "status": 201, "data": {"docs_id": 1}}, status=status.HTTP_201_CREATED)
+
+        ####################################################
+        if repository_url.startswith("https://"):
+            repository_url = repository_url.replace("https://", "")
+
+        repo_url_list = repository_url.split("/")
+        owner = repo_url_list[1]
+        repo = repo_url_list[2].split(".")[0]
+        path = ''
+        root_file = get_file_content(owner, repo, path)
+
+        # TODO: 찾아낸 Framework를 활용하여 GitHub 코드 추출
+        if root_file:
+            prompt_ary = get_github_code_prompt(repository_url, framework)
+            response, stack, res_title = get_assistant_response(prompt_ary, language)
+
+        request.data['user_id'] = User.objects.filter(id=user_id).first().id
+        request.data['title'] = res_title
+        request.data['content'] = response
+        request.data['language'] = language
+        request.data['color'] = color
+        # TODO: Database에 docs 저장 후
         serializer = DocsSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            docs = serializer.save()
+        else:
+            return Response({"message": "문서 생성 실패", "status": 500}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # TODO: tech_stack에 기술 저장
+        stack_ary = []
+
+        if stack in ',':
+            stack_ary = stack.split(', ')
+        else:
+            stack_ary = stack.split('/')
+
+        res_data = {
+            "docs_id": docs.id,
+            "title": docs.title,
+            "content": docs.content,
+            "language": docs.language,
+            "tech_stack": stack_ary,
+            "color": docs.color,
+            "created_at": docs.created_at,
+        }
+        return Response({"message": "문서 생성 성공", "status": 201, "data": res_data}, status=status.HTTP_201_CREATED)
+        # TODO: 추출한 코드를 활용하여 문서 생성
+
+
+
 class DocsShareView(APIView):
     permission_classes = [IsAuthenticated]
     @swagger_auto_schema(request_body=SwaggerDocsSharePostSerializer)
@@ -120,35 +168,6 @@ class DocsShareView(APIView):
             serializer.save()
             return serializer.response(data=serializer.data)
         return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-@api_view(['POST'])
-def docs_share(request):
-    if request.method == 'POST':
-        docs_id = request.data.get('docs_id')
-
-        if docs_id is None:
-            return Response({"message": "문서 ID를 입력해 주세요.", "status": 400}, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            doc = Docs.objects.get(pk=docs_id)
-        except Docs.DoesNotExist:
-            return Response({"message": "존재하지 않는 문서 ID입니다.", "status": 404}, status=status.HTTP_404_NOT_FOUND)
-
-        if doc.url is not None:
-            return Response({"message": "이미 URL이 생성된 문서입니다.", "status": 409, "existing_url": doc.url}, status=status.HTTP_409_CONFLICT)
-
-        # UUID를 사용하여 고유한 URL 생성
-        base_url = 'http://127.0.0.1:8000/api/v1/docs/share/'
-        unique_url = base_url + str(uuid.uuid4())
-        doc.url = unique_url
-        doc.save()
-
-        return Response({"message": "문서 공유 URL 생성 성공", "status": 201, "data": {"url": doc.url}}, status=status.HTTP_201_CREATED)
-
-
-        doc.url = unique_url
-            return Response({"message": "문서 공유 URL 생성 성공", "status": 201, "data": {"url": doc.url}},
-                            status=status.HTTP_201_CREATED)
 
 
 class DocsContributorView(APIView):
