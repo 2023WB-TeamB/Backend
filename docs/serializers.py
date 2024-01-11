525a59bfba6b5b90c37a4eb8a2d6ea87766b1b1e
kimsunjae <woal9844@gmail.com>
2024-01-11 19:52:18 +0900

    style : 들여쓰기 수정

--- docs/serializers.py
+++ docs/serializers.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-
 class DocsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Docs
@@ -30,10 +29,10 @@ class DocsSerializer(serializers.ModelSerializer):
         doc = Docs.objects.get(pk=docs_id)
 
         response_data = {
-            "message" : "문서 공유 URL 생성 성공",
-            "status" : 201,
+            "message": "문서 공유 URL 생성 성공",
+            "status": 201,
             "data": {
-            "share_url" : doc.url,
+                "share_url": doc.url,
             }
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
