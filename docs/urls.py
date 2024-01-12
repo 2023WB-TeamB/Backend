e7925836861f07321d8c1607454123ab3dd41448
김동현 <kdh@MacBookPro.local>
2024-01-12 21:05:52 +0900

    feat: 문서 상세 조회

--- docs/urls.py
+++ docs/urls.py
@@ -3,9 +3,8 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    # path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
     path('docs/', views.DocsList.as_view()),  # get - 문서 내역 조회
-    # path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
+    path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
     path("docs/create/", DocsCreateView.as_view()),
     path("docs/share/", DocsShareView.as_view()),
     path('docs/contributor/',DocsContributorView.as_view()),
