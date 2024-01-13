5f285f48f50ba5805ce2471a6c138db3737c4efb
SNOW <kancth03@gmail.com>
2024-01-13 09:30:41 +0900

    feat: badge app settings 설정

--- gtd/settings.py
+++ gtd/settings.py
@@ -147,17 +147,23 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gtd.urls'
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
             ],
         },
     },
