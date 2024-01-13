6a9981ddbb83135c0c1688a26e1e80ae1bc6bb7a
SNOW <kancth03@gmail.com>
2024-01-13 18:18:06 +0900

    fix: 기능 구현을 위해 엔드포인트 수정

--- badge/urls.py
+++ badge/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('badge/<str:github_user>/<str:repo_name>/', views.BadgeView.as_view()),  # get - 문서 내역 조회
+    path('badge/<str:github_user_organization>/<str:repo_name>/<str:github_user>', views.BadgeView.as_view()),  # get - 문서 내역 조회
 ]
