320d26f207a17113eca0840a2bc6aff3a71d5519
SNOW <kancth03@gmail.com>
2024-01-13 18:22:13 +0900

    feat: GitHub data 분석 로직 구현

--- badge/views.py
+++ badge/views.py
@@ -37,15 +37,59 @@ class BadgeView(APIView):
         env = environ.Env(DEBUG=(bool, True))
         GITHUB_TOKEN = env('GITHUB_TOKEN')
         headers = {'Authorization': 'token ' + GITHUB_TOKEN}
+
+        repository_url = "https://github.com/" + github_user_organization + "/" + repo_name
         # 뱃지에 등록된 데이터가 있는 경우
         if Badge.objects.filter(github_id=github_user, repository_url=repository_url).exists():
             badge = Badge.objects.get(github_id=github_user, repository_url=repository_url)
 
         # DataBase에 Badge Data가 없는 경우
         else:
+
             # TODO: Github Repository 유효성 검사
+            try:
+                result = requests.get(f"https://api.github.com/repos/{github_user_organization}/{repo_name}/contributors", headers=headers)
+            except:
+                return Response({'message': '유효한 레포지토리 URL을 입력해 주세요.', 'status': 404}, status=status.HTTP_404_NOT_FOUND)
+
+            if result.status_code != 200:
+                return Response({'message': '유효한 레포지토리 URL을 입력해 주세요.', 'status': 404}, status=status.HTTP_404_NOT_FOUND)
+
             # TODO: 유효한 경우 Repository Contributor에 user가 있는지 검사
+            contributors = json.loads(result.text)
+            if not any(contributor['login'] == github_user for contributor in contributors):
+                return Response({'message': f'{github_user}님은 {repo_name}에 Contributor로 등록되지 않았습니다.', 'status': 400}, status=status.HTTP_400_BAD_REQUEST)
+
             # TODO: GitHub Logic을 통해 데이터 받아오기
+            pr_response = requests.get(f"https://api.github.com/repos/{github_user_organization}/{repo_name}/pulls?state=all", headers=headers)
+
+            if pr_response.status_code != 200:
+                return Response({'message': 'GitHub API Server Error.', 'status': 500}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            #######################################################################################################
+
+            pull_request_list = json.loads(pr_response.text)
+            total_contributions = sum([contributor['contributions'] for contributor in contributors])
+            result = []
+
+            for contributor in contributors:
+                user_prs = [pr for pr in pull_request_list if pr['user']['login'] == contributor['login']]
+                contribution_percent = '{:.2f}%'.format((contributor['contributions'] / total_contributions) * 100)
+                res_data = {
+                    'contributor': contributor['login'],
+                    'commit_count': contributor['contributions'],
+                    'contribution_percent': contribution_percent,
+                    'pr_count': len(user_prs),
+                }
+                # DataBase에 저장
+                badge = Badge(github_id=res_data['contributor'],
+                              commit_cnt=res_data['commit_count'],
+                              pull_request_cnt=res_data['pr_count'],
+                              contribution=res_data['contribution_percent'].strip("%"),
+                              repository_url=repository_url)
+                badge.save()
+                result.append(res_data)
+
+            # print(request_contributor_info)
             # -------------------------------------------------------------------------
             # 임시 데이터 (GitHub Logic 구현 후 변경 필요)
             commit_cnt = 230
