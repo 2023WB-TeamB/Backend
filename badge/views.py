7cc686c3876a860475e3901ba4c5a88b664c210b
SNOW <kancth03@gmail.com>
2024-01-13 09:31:33 +0900

    feat: 1차 디자인 및 기능 구현

--- badge/views.py
+++ badge/views.py
@@ -1,6 +1,21 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.template.response import TemplateResponse
+from django.http import HttpResponse
+from django.template import loader
 
 
 class BadgeView(APIView):
-    def get(self, github_id, repo_name):
-        pass
+    def get(self, request, github_user, repo_name, *args, **kwargs):
+        print(github_user, repo_name)
+
+        theme = request.GET.get('theme', 'basic')
+        template = loader.get_template('tag/profile.html')
+        context = {'github_user': github_user, 'repo_name': repo_name, 'percent': 80}
+        print(context)
+        response = HttpResponse(content=template.render(context, request))
+        response['Content-Type'] = 'image/svg+xml'
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        return response
