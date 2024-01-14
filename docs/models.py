cd783b8f5c4ee7f114e3d0ab9f06048e481f72d4
김동현 <kdh@MacBookPro.local>
2024-01-15 02:30:36 +0900

    fix: TechStack model 추가

--- docs/models.py
+++ docs/models.py
@@ -11,10 +11,21 @@ class Docs(models.Model):
     repository_url = models.URLField(max_length=1000)
     url = models.URLField(max_length=1000, null=True)
     language = models.CharField(max_length=30)
-    color = models.CharField(max_length=7)
+    color = models.CharField(max_length=7, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+class TechStack(models.Model):
+    id = models.AutoField(primary_key=True)
+    docs_id = models.ForeignKey(Docs, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
