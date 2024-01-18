55d96e4e81527a3fa25bcfdfd2730485b631269e
HongSeonah <rse1186@naver.com>
2024-01-18 16:27:02 +0900

    chore: redis에 refresh token 저장 및 삭제

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
-# Create your views here.
+
 import jwt
 
 from gtd.settings import *
@@ -12,6 +12,8 @@ from rest_framework.response import Response
 
 from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
+from django.core.cache import cache
+from rest_framework_simplejwt.tokens import UntypedToken
 
 # swagger 관련
 from rest_framework.views import APIView
@@ -19,7 +21,6 @@ from drf_yasg.utils import swagger_auto_schema
 
 
 # from drf_yasg import openapi
-
 
 class RegisterAPIView(APIView):
     @swagger_auto_schema(request_body=SwaggerRegisterPostSerializer)
@@ -86,12 +87,11 @@ class AuthAPIView(APIView):
             serializer = SignSerializer(instance=user)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-        except(jwt.exceptions.ExpiredSignatureError):
+        except jwt.exceptions.ExpiredSignatureError:
             # 토큰 만료 시 토큰 갱신
-
             data = {'refresh': request.COOKIES.get('refresh', None)}
             serializer = TokenRefreshSerializer(data=data)
+
             if serializer.is_valid(raise_exception=True):
                 access = serializer.data.get('access', None)
                 refresh = serializer.data.get('refresh', None)
@@ -99,41 +99,56 @@ class AuthAPIView(APIView):
                 pk = payload.get('user_id')
                 user = get_object_or_404(User, pk=pk)
                 serializer = SignSerializer(instance=user)
+
                 res = Response(serializer.data, status=status.HTTP_200_OK)
                 res.set_cookie('access', access)
                 res.set_cookie('refresh', refresh)
                 return res
+
             raise jwt.exceptions.InvalidTokenError
 
-        except(jwt.exceptions.InvalidTokenError):
+        except jwt.exceptions.InvalidTokenError:
             # 사용 불가능한 토큰
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    @swagger_auto_schema(request_body=SwaggerLoginPostSerializer)
+    def put(self, request):
+        flag = request.data.get('flag')
+        key = request.data.get('key')
+
+        if flag == 'set':
+            value = request.data.get('value')
+            cache.set(key, value)
+        elif flag == 'get':
+            res = cache.get(key)
+            return Response({key : res}, status=status.HTTP_200_OK)
+
+        return Response({'msg': 'success'}, status=status.HTTP_200_OK)
+
     # 로그인
     def post(self, request):
-        # 유저 인증
         user = authenticate(
             email=request.data.get("email"), password=request.data.get("password")
         )
         if user is not None:
             serializer = SignSerializer(user)
-            # 토큰 접근
             token = TokenObtainPairSerializer.get_token(user)
             refresh_token = str(token)
             access_token = str(token.access_token)
+
+            # 리프레쉬 토큰을 Redis에 저장
+            cache.set(user.id, refresh_token, timeout=int(token.lifetime.total_seconds()))
+
             res = Response(
                 {
-                    # "user": serializer.data,
                     "message": "login success",
                     "token": {
                         "access": access_token,
                         "refresh": refresh_token,
                     },
+                    "user_id": user.id,
                 },
                 status=status.HTTP_200_OK,
             )
-            # 토큰, 쿠키에 저장
             res.set_cookie("access", access_token, httponly=True)
             res.set_cookie("refresh", refresh_token, httponly=True)
             return res
@@ -142,10 +157,26 @@ class AuthAPIView(APIView):
 
     # 로그아웃
     def delete(self, request):
+        access_token = request.COOKIES.get('access')
+        refresh_token = request.COOKIES.get('refresh')
+        user = User.objects.filter(email=request.data.get("email")).first()
+
+        # access 토큰 검증
+        try:
+            UntypedToken(access_token)
+        except jwt.exceptions.InvalidTokenError:
+            return Response({'message': 'Invalid token.'}, status=status.HTTP_400_BAD_REQUEST)
+
         # 쿠키에 저장된 토큰 삭제
         response = Response({
             "message": "Logout success"
         }, status=status.HTTP_202_ACCEPTED)
         response.delete_cookie("access")
         response.delete_cookie("refresh")
+
+        # Redis에서 리프레쉬 토큰 삭제
+        cache.delete(user.id)
+
         return response
+
+
