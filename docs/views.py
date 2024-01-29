bd0831f189afefb5a69c03b882bf01d86f35ae59
kimsunjae <woal9844@gmail.com>
2024-01-29 17:05:34 +0900

    fix : delete S3img model

--- docs/views.py
+++ docs/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from users.utils import user_token_to_data
-from .models import Docs, User, Keywords, S3Img
+from .models import Docs, User, Keywords
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
