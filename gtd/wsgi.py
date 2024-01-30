65e7ff9606565748c7d07f57e3f64d15fa55637c
kimsunjae <woal9844@gmail.com>
2024-01-31 03:45:28 +0900

    hotfix : gevent 오류해결

--- gtd/wsgi.py
+++ gtd/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+import gevent.monkey
+gevent.monkey.patch_all()
 import os
 
 from django.core.wsgi import get_wsgi_application
